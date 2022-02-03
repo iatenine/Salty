0234d461fd9ed9e487767571be3110e7dc6804df
Jack <iatenine@gmail.com>
2022-02-03 16:11:06 -0700

    Models Refactored Now using Project Lombok and Models hold references to one another when appropriate for the purposes of foreign keys (1-to-x relationships). Closes #12

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -1,31 +1,23 @@
 package models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Customer
  * Model representing customer table in db. Only holds name and a foreign key to access
  * sensitive information
  */
-
+@AllArgsConstructor
+@Getter
 public class Customer {
     final private int id;
     final private String customer_name;
-    final private int customer_data_id;
+    final private CustomerData customer_data;
 
-    public Customer(int id, String customer_name, int customer_data_id) {
+    public Customer(int id, String customer_name){
         this.id = id;
         this.customer_name = customer_name;
-        this.customer_data_id = customer_data_id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getCustomer_name() {
-        return customer_name;
-    }
-
-    public int getCustomer_data_id() {
-        return customer_data_id;
+        this.customer_data = null;
     }
 }
--- src/main/java/models/Item.java
+++ src/main/java/models/Item.java
@@ -1,31 +1,13 @@
 package models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Item {
     final private int id;
     final private String name;
     final private int price;
     final private boolean available;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public Item(int id, String name, int price, boolean available) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.available = available;
-    }
 }
