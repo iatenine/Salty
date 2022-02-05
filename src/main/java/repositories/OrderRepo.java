ace4a8de109d13ddf84bf99cb2f765db5cbaa70b
Jack <iatenine@gmail.com>
2022-02-05 10:17:54 -0700

    Implemented item service Closes #24

--- src/main/java/repositories/OrderRepo.java
+++ src/main/java/repositories/OrderRepo.java
@@ -1,5 +1,24 @@
 package repositories;
 
-public class OrderRepo {
-    
+import models.Order;
+
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+public class OrderRepo extends SaltyRepo<Order>{
+
+    @Override
+    public Order save(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order getById(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public LinkedList<Order> getAll() throws SQLException {
+        return null;
+    }
 }
